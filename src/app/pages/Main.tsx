203d55429d512ed1e15c4526e35705e5b1158cb7
백원만 <baekwon10000@gmail.com>
2023-10-27 23:07:50 +0900

    싸이트 소개글 업데이트

--- src/app/pages/Main.tsx
+++ src/app/pages/Main.tsx
@@ -21,8 +21,12 @@ class Main extends React.Component<Props, {}> {
         <div className="row">
           <div className="col-md-12 text-center">
             <p>
-              Holy shit, ad sites!
-              It's dirty and cheesy, so I'd rather make it and use it.
+              내가 만들어서 쓰는 나만의 툴 싸이트.<br/>
+              광고 떡칠 개같은 싸이트들이 만드는 계기 한몫했다.<br/>
+              나도 광고 좀 붙여서 용돈 벌이 좀 하고 싶었는데 가망이 안보여서 그만둠.<br/>
+              My own tool site that I create and use.<br/>
+              Advertising sites played a role in creating this.<br/>
+              I also wanted to earn some pocket money by putting up some ads, but I didn't see any hope so I quit.
             </p>
           </div>
         </div>
