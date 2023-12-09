95ab8c194aa0939342254773fcd5e0098054922b
백원만 <baekwon10000@gmail.com>
2023-12-09 23:03:44 +0900

    문구수정

--- src/app/pages/Main.tsx
+++ src/app/pages/Main.tsx
@@ -22,7 +22,7 @@ class Main extends React.Component<Props, {}> {
           <div className="col-md-12 text-center">
             <p>
               내가 만들어서 쓰는 나만의 툴 싸이트.<br/>
-              광고 떡칠 개같은 싸이트들이 만드는 계기 한몫했다.<br/>
+              광고 떡칠 x같은 싸이트들이 만드는 계기 한몫했다.<br/>
               나도 광고 좀 붙여서 용돈 벌이 좀 하고 싶었는데 가망이 안보여서 그만둠.<br/>
               My own tool site that I create and use.<br/>
               Advertising sites played a role in creating this.<br/>
